835d9e4ad7dc3cbd991e225875545fa9379c561f
Mark_Mutunga <markmiki03@gmail.com>
2025-07-23 13:41:18 +0300

    Add comprehensive copyright headers to all source files
    - Added copyright notice to all Kotlin source files (.kt)
    - Added copyright notice to AndroidManifest.xml and build.gradle
    - Copyright includes Mark Mikile Mutunga's contact information
    - Email: markmiki03@gmail.com
    - Phone: +254 707 678 643
    - All rights reserved with proper legal disclaimer
    - Build verification: All files compile successfully

--- app/src/main/java/com/safenet/shield/utils/SecurityUtils.kt
+++ app/src/main/java/com/safenet/shield/utils/SecurityUtils.kt
@@ -1,3 +1,22 @@
+/*
+ * SafeNet Shield - Personal Safety & Security Application
+ * 
+ * Copyright (c) 2024 Mark Mikile Mutunga
+ * Email: markmiki03@gmail.com
+ * Phone: +254 707 678 643
+ * 
+ * All rights reserved. This software and associated documentation files (the "Software"),
+ * are proprietary to Mark Mikile Mutunga. Unauthorized copying, distribution, or modification
+ * of this software is strictly prohibited without explicit written permission from the author.
+ * 
+ * This software is provided "as is", without warranty of any kind, express or implied,
+ * including but not limited to the warranties of merchantability, fitness for a particular
+ * purpose and noninfringement. In no event shall the author be liable for any claim,
+ * damages or other liability, whether in an action of contract, tort or otherwise,
+ * arising from, out of or in connection with the software or the use or other dealings
+ * in the software.
+ */
+
 package com.safenet.shield.utils
 
 import android.content.Context
